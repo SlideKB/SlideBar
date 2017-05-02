48d8513f4ce8121efc2d3b1c38adf97d6de32cbb
PureSpider <alex.lamprecht1@gmail.com>
2017-05-02 13:21:38 +0200

    Some optimizations
    Use java 8 streams; remove the duplicate List - use Map.values() instead

--- src/main/java/com/github/slidekb/back/PortManager.java
+++ src/main/java/com/github/slidekb/back/PortManager.java
@@ -27,53 +27,50 @@ import java.util.HashMap;
  */
 public class PortManager {
 
-    private ArrayList<Arduino> arduinos = new ArrayList<>();
-    
-	private ObservableList<Object> portList;
-	
-	HashMap<String, Arduino> arduinoHash = new HashMap<String, Arduino>();
+    private ObservableList<Object> portList;
 
-    public PortManager(){
+    HashMap<String, Arduino> arduinoHash = new HashMap<String, Arduino>();
+
+    public PortManager() {
 
     }
 
-    protected boolean addArduino(String Port){
+    protected boolean addArduino(String Port) {
         Arduino temp = new Arduino(Port);
         temp.initialize();
-        if (temp.isConnectedAndSlider()){
-            arduinos.add(temp);
+        if (temp.isConnectedAndSlider()) {
             arduinoHash.put(temp.getID(), temp);
-            if (arduinos.size() == 1){
-            	arduinoHash.put("default", temp);
+            if (arduinoHash.size() == 1) {
+                arduinoHash.put("default", temp);
             }
         }
         return temp.isConnectedAndSlider();
     }
-    
-    public Arduino getArduinoFromID(String ID){
-		return arduinoHash.get(ID);
-    }
-    
-    public String[] getAllArduinoID(){
-    	String[] temp = new String[arduinos.size()];
-    	for (int i = 0; i < arduinos.size(); i++){
-    		temp[i] = arduinos.get(i).getID();
-    	}
-    	return temp;
+
+    public Arduino getArduinoFromID(String ID) {
+        return arduinoHash.get(ID);
     }
 
-    public Arduino[] getArduinos(){
-        return arduinos.toArray(new Arduino[arduinos.size()]);
+    public String[] getAllArduinoID() {
+        return arduinoHash //
+                .values() // Get the Collection of all values of the Map
+                .stream() // Construct a stream from it
+                .map(entry -> entry.getID()) // Transform all the entries in the stream
+                .toArray(size -> new String[size]); // Collect the entries into an array
     }
 
-    public void findAndConnect(){
-        for (String s: getPortList(0)){
+    public Arduino[] getArduinos() {
+        return arduinoHash.values().toArray(new Arduino[arduinoHash.size()]);
+    }
+
+    public void findAndConnect() {
+        for (String s : getPortList(0)) {
             addArduino(s);
         }
     }
 
     public static String[] getPortList(int index) {
-    	System.out.println("Getting port list");
-    	return SerialPortList.getPortNames();
+        System.out.println("Getting port list");
+        return SerialPortList.getPortNames();
     }
 }
