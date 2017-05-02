6a65c97686b89f0c077a620f31e0045ad645da85
PureSpider <alex.lamprecht1@gmail.com>
2017-05-02 21:51:56 +0200

    Fix test

--- src/test/java/testing/SlideBarConnectionTest.java
+++ src/test/java/testing/SlideBarConnectionTest.java
@@ -7,23 +7,23 @@ import com.github.slidekb.back.PortManager;
  * Created by JackSec on 4/27/2017.
  */
 public class SlideBarConnectionTest {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         PortManager pm = new PortManager();
         pm.findAndConnect();
-        for (Arduino a: pm.getArduinos()){
+        for (Arduino a : pm.getArduinos().values()) {
             a.getID();
             a.getPortName();
             a.writeUntilComplete(512);
             a.writeUntilComplete(100);
             a.writeUntilComplete(1000);
         }
-        for (Arduino a: pm.getArduinos()){
+        for (Arduino a : pm.getArduinos().values()) {
             a.writeUntilComplete(512);
         }
-        for (Arduino a: pm.getArduinos()){
+        for (Arduino a : pm.getArduinos().values()) {
             a.writeUntilComplete(100);
         }
-        for (Arduino a: pm.getArduinos()){
+        for (Arduino a : pm.getArduinos().values()) {
             a.writeUntilComplete(1000);
         }
     }
