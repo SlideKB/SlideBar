4a04b18d3b984acb6e1e2f007953016957bfa96f
Jack Kester <Jackrabbit.kester@gmail.com>
2017-06-06 07:29:22 -0500

    added test support for Mac

--- src/test/java/unitTests/ArduinoConnectionTest.java
+++ src/test/java/unitTests/ArduinoConnectionTest.java
@@ -2,18 +2,37 @@ package unitTests;
 
 import static org.junit.Assert.*;
 
+import java.util.regex.Pattern;
+
 import org.junit.Test;
 
 import com.github.slidekb.back.Arduino;
+
+import jssc.SerialPortList;
 
 public class ArduinoConnectionTest {
 
 	@Test
 	public void connectToArduinoTest1(){
 		boolean sliderFound = false;
-		for (int i = 1; i < 30; i++){
+		String[] portNames = SerialPortList.getPortNames();
+		for (String port : portNames){
 			try {
-				Arduino a = new Arduino("COM"+i);
+				Arduino a = new Arduino(port);
+				a.initialize();
+				if (a.getID() != null){
+					sliderFound = true;
+					System.out.println(a.getID());
+				}
+			} catch (Exception e){
+				//do nothing
+			}
+			
+		}
+		portNames = SerialPortList.getPortNames("/dev/", Pattern.compile("tty.*"));
+		for (String port : portNames){
+			try {
+				Arduino a = new Arduino(port);
 				a.initialize();
 				if (a.getID() != null){
 					sliderFound = true;
