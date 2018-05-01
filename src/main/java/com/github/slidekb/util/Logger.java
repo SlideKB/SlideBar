ee44d6afa019964ebcc8b16f62e9f6de6ba41e00
JACKATK-PC\Jackatk <jack@jackkester.com>
2018-05-01 16:54:40 -0500

    added logger class

---
+++ src/main/java/com/github/slidekb/util/Logger.java
@@ -0,0 +1,19 @@
+package com.github.slidekb.util;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Logger {
+	public static void logMessage(String message) {
+		PrintWriter out;
+		try {
+			out = new PrintWriter(new FileWriter("output.txt", true), true);
+			out.write(message);
+			out.close();
+		} catch (IOException e) {
+			System.out.println("Logger->void()-> could not write to file");
+		}
+
+	}
+}
